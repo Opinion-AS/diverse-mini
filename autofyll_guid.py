50caa3f8d26d096ef980e11918d3bf5af2e3c20c
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2024-02-05 16:19:13 +0100

    Update autofyll_guid.py

--- autofyll_guid.py
+++ autofyll_guid.py
@@ -1,29 +1,41 @@
 
 import pyautogui
+import time
 
-alt_texts = ["a7696d72-913e-4b57-8898-9add7fe5f18e", "7f072cfa-d53e-46c0-bdd7-e45efd56fc93"] # GUID
+
+alt_texts = ["4c92ddb9-2aa7-46d9-ab6d-0c6cd1548b3e", "58ad5a18-448f-4db3-be17-e88ed5cf2420"] # GUID
 
 for i in alt_texts:
-    pyautogui.moveTo(x=897, y=525) # Plott
+    pyautogui.moveTo(x=994, y=457) # Plott
+    time.sleep(0.5)
     pyautogui.click()
     pyautogui.moveTo(x=1577, y=616) # Tekstboks
+    time.sleep(0.5)
     pyautogui.click()
     pyautogui.write(i)
-    pyautogui.moveTo(x=424, y=946) # Footer
+    pyautogui.moveTo(x=424, y=935) # Footer
+    time.sleep(0.5)
     pyautogui.click()
     pyautogui.moveTo(x=1577, y=616) # Tekstboks
+    time.sleep(0.5)
     pyautogui.click()
     pyautogui.write(f"Footer: {i}")
-    pyautogui.moveTo(x=599, y=934) # Title 
+    pyautogui.moveTo(x=1123, y=920) # Title 
+    time.sleep(0.5)
     pyautogui.click()
     pyautogui.moveTo(x=1577, y=616) # Tekstboks
+    time.sleep(0.5)
     pyautogui.click()
     pyautogui.write(f"Title: {i}")
-    pyautogui.moveTo(x=1013, y=444) # Header
+    pyautogui.moveTo(x=1013, y=344) # Header
+    time.sleep(0.5)
     pyautogui.click()
     pyautogui.moveTo(x=1577, y=616) # Tekstboks
+    time.sleep(0.5)
     pyautogui.click()
     pyautogui.write(f"Title: {i}")
-    pyautogui.moveTo(x=1116, y=608)
+    pyautogui.moveTo(x=1439, y=542)
+    time.sleep(0.5)
     pyautogui.click()
     pyautogui.press("pagedown") # Neste
+ 
